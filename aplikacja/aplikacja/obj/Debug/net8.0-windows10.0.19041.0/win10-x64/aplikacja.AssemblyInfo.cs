72c6791e038ea6b2657e003dc628ab2a7bf1eaea
Pawel-dud <pawel_dudziak@spoko.pl>
2024-11-10 15:28:19 +0100

    Settings dodanie przycisku select language

--- aplikacja/aplikacja/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/aplikacja.AssemblyInfo.cs
+++ aplikacja/aplikacja/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/aplikacja.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aplikacja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30065f8aafc76a6a2cc4168e6f20791a5c6361ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bff9583d5ce88be4e4f0979958febd3f8fdc4af2")]
 [assembly: System.Reflection.AssemblyProductAttribute("aplikacja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aplikacja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- aplikacja/aplikacja/strony/zakladki/zakladka3.xaml.cs
+++ aplikacja/aplikacja/strony/zakladki/zakladka3.xaml.cs
@@ -12,5 +12,25 @@ public partial class zakladka3 : ContentPage
     public zakladka3()
     {
         InitializeComponent();
+        //lista jêzyków
+        List<string> languages = new List<string> { "English", "Polski" };
+        LanguageListView.ItemsSource = languages;
+    }
+
+    private void OnSelectLanguageButtonClicked(object sender, EventArgs e)
+    {
+        // pokazuje lub ukrywa listy jêzyków po klikniêciu przycisku
+        LanguageListView.IsVisible = !LanguageListView.IsVisible;
+    }
+
+    private void OnLanguageSelected(object sender, SelectionChangedEventArgs e)
+    {
+        string selectedLanguage = e.CurrentSelection[0] as string;
+
+        // Tutaj dodaj logikê zmiany jêzyka (np. zapisanie preferencji u¿ytkownika)
+        DisplayAlert("Language Selected", $"Selected language: {selectedLanguage}", "OK");
+
+        // ukrywa listê po dokonaniu wyboru
+        LanguageListView.IsVisible = false;
     }
 }
