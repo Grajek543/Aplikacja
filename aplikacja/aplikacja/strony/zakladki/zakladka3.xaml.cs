bff9583d5ce88be4e4f0979958febd3f8fdc4af2
Pawel-dud <pawel_dudziak@spoko.pl>
2024-11-10 15:11:04 +0100

    Zakladka settings sam tytul

---
+++ aplikacja/aplikacja/strony/zakladki/zakladka3.xaml.cs
@@ -0,0 +1,16 @@
+using aplikacja.Models;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace aplikacja.strony.zakladki;
+
+public partial class zakladka3 : ContentPage
+{
+    public zakladka3()
+    {
+        InitializeComponent();
+    }
+}
