068a4f0b8074efa22933fc88345ad9d38ff9361c
Grajek543 <gfiejtek@gmail.com>
2025-01-03 22:15:45 +0100

    Działający FirebaseAuth

--- PARTPICKER/PARTPICKER/strony/zakladki/Rejestracja.xaml.cs
+++ PARTPICKER/PARTPICKER/strony/zakladki/Rejestracja.xaml.cs
@@ -1,4 +1,5 @@
 namespace PARTPICKER.strony.zakladki;
+using Plugin.FirebaseAuth;
 
 public partial class Rejestracja : ContentPage
 {
@@ -9,5 +10,22 @@ public partial class Rejestracja : ContentPage
 	async void closebutton(object sender, EventArgs args)
     {
         Navigation.PopModalAsync();
+    }
+
+    async void OnRegisterClicked(object sender, EventArgs e)
+    {
+        string email = EmailEntry.Text; // Pobiera e-mail z pola
+        string password = PasswordEntry.Text; // Pobiera has³o z pola
+
+        try
+        {
+            var user = await CrossFirebaseAuth.Current.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+            await DisplayAlert("Sukces", $"Utworzono konto: {user.User.Email}!", "OK");
+            await Navigation.PopAsync(); // Powrót na ekran logowania
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("B³¹d", ex.Message, "OK");
+        }
     }
 }
--- PARTPICKER/PARTPICKER/strony/zakladki/zakladka4.xaml.cs
+++ PARTPICKER/PARTPICKER/strony/zakladki/zakladka4.xaml.cs
@@ -1,4 +1,6 @@
 namespace PARTPICKER.strony.zakladki;
+using Plugin.FirebaseAuth;
+using System;
 
 public partial class zakladka4 : ContentPage
 {
@@ -9,5 +11,21 @@ public partial class zakladka4 : ContentPage
     async void OnButtonClicked(object sender, EventArgs args)
     {
         Navigation.PushModalAsync(new Rejestracja());
+    }
+
+    async void OnLoginClicked(object sender, EventArgs args)
+    {
+        string email = EmailEntry.Text; // Pobiera e-mail z pola wejœciowego
+        string password = PasswordEntry.Text; // Pobiera has³o z pola wejœciowego
+
+        try
+        {
+            var user = await CrossFirebaseAuth.Current.Instance.SignInWithEmailAndPasswordAsync(email, password);
+            await DisplayAlert("Sukces", $"Witaj, {user.User.Email}!", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("B³¹d", ex.Message, "OK");
+        }
     }
 }
